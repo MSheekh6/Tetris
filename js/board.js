ae64b3aff57afaf5d09726a2a8aa000338dcdaf4
Mohammed Sheekh-Amin <121132867+MSheekh6@users.noreply.github.com>
2025-10-22 14:39:19 +0100

    Add width and height properties to Board class

--- js/board.js
+++ js/board.js
@@ -1,5 +1,9 @@
 export class Board {
     constructor(width = 10, height = 20) {
+        this.width = width;
+        this.height = height;
+        this.grid = this.createEmptyGrid();
+    }
 
     createEmptyGrid() {
         return Array(this.height).fill(null).map(() => Array(this.width).fill(0));
